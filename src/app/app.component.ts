31144881216771769afa1df7a4ea3fb5e0ba5aa8
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-12-24 13:30:13 -0800

    binding properties

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  serverElements = [];
+  serverElements = [{ type: 'server', name: 'test', content: 'this is a test!'}];
 
 }
